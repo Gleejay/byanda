0ce327b38628647da14c78c07377de7afca7c01d
lvzuheng <289019372@qq.com>
2016-11-24 17:22:42 +0800

    aaaaasssssssss

--- Test/src/cn/MyTest.java
+++ Test/src/cn/MyTest.java
@@ -2,12 +2,15 @@ package cn;
 
 import java.util.Arrays;
 
+import org.apache.commons.lang.SystemUtils;
+
 public class MyTest {
 	public static void main(String args[]) {
 		
 		System.out.println("gaigaigai");
 		System.out.println("fdsgsg");
-		
+		//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+		System.out.println("today");
 	}
 
 }
